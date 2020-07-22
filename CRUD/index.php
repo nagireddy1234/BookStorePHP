e0ad0390ac0b258caf40f3db1d43ab0aff89826a
nagiReddy <nagireddy.panditi@gmail.com>
2020-07-23 01:52:21 +0530

    changes added

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,63 +1,109 @@
+<?php
 
-<?php 
- require_once("./php/component.php");
+require_once ("./php/component.php");
+require_once ("./php/operation.php");
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Book Store</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-  <!-- Custom StyleSheet -->
-  <link rel="stylesheet" href="styles/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Books</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
 </head>
-
 <body>
-  <main>
+
+<main>
     <div class="container text-center">
-      <h1 class="py-4 bg-dark text-light rounded"><i class='fas fa-swatchbook'></i>Book Store</h1>
+        <h1 class="py-4 bg-dark text-light rounded"><i class="fas fa-swatchbook"></i> Book Store</h1>
 
-      <div class="d-flex justify-content-center">
-        <form action="" method="post" class="w-50">
-        <div class="input-group py-2">
-        <div class="input-group-prepend">
-          <span class="input-group-text bg-warning" id="basic-addon1"><i class='fas fa-swatchbook'></i></span>
-         </div>
-          <input type="text" class="form-control" autocomplete="off" placeholder="ID" aria-label="Username" aria-describedby="basic-addon1">
+        <div class="d-flex justify-content-center">
+            <form action="" method="post" class="w-50">
+                <div class="pt-2">
+                    <?php inputElement("<i class='fas fa-id-badge'></i>","ID", "book_id",setID()); ?>
+                </div>
+                <div class="pt-2">
+                    <?php inputElement("<i class='fas fa-book'></i>","Book Name", "book_name",""); ?>
+                </div>
+                <div class="row pt-2">
+                    <div class="col">
+                        <?php inputElement("<i class='fas fa-people-carry'></i>","Publisher", "book_publisher",""); ?>
+                    </div>
+                    <div class="col">
+                        <?php inputElement("<i class='fas fa-dollar-sign'></i>","Price", "book_price",""); ?>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                        <?php buttonElement("btn-create","btn btn-success","<i class='fas fa-plus'></i>","create","data-toggle='tooltip' data-placement='bottom' title='Create'"); ?>
+                        <?php buttonElement("btn-read","btn btn-primary","<i class='fas fa-sync'></i>","read","data-toggle='tooltip' data-placement='bottom' title='Read'"); ?>
+                        <?php buttonElement("btn-update","btn btn-light border","<i class='fas fa-pen-alt'></i>","update","data-toggle='tooltip' data-placement='bottom' title='Update'"); ?>
+                        <?php buttonElement("btn-delete","btn btn-danger","<i class='fas fa-trash-alt'></i>","delete","data-toggle='tooltip' data-placement='bottom' title='Delete'"); ?>
+                        <?php deleteBtn();?>
+                </div>
+            </form>
         </div>
 
-        <div class="pt-2">
-          <?php inputElement(icon:"<i class='fas fa-swatchbook'></i>", placeholder:"Book Name", name:"book_name", value:"");
-          ?>
+        <!-- Bootstrap table  -->
+        <div class="d-flex table-data">
+            <table class="table table-striped table-dark">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>Book Name</th>
+                        <th>Publisher</th>
+                        <th>Book Price</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+                <tbody id="tbody">
+                   <?php
 
+
+                   if(isset($_POST['read'])){
+                       $result = getData();
+
+                       if($result){
+
+                           while ($row = mysqli_fetch_assoc($result)){ ?>
+
+                               <tr>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['id']; ?></td>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['book_name']; ?></td>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['book_publisher']; ?></td>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo '$' . $row['book_price']; ?></td>
+                                   <td ><i class="fas fa-edit btnedit" data-id="<?php echo $row['id']; ?>"></i></td>
+                               </tr>
+
+                   <?php
+                           }
+
+                       }
+                   }
+
+
+                   ?>
+                </tbody>
+            </table>
         </div>
-        </form>
-      </div>
+
 
     </div>
-  </main>
+</main>
 
 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-
-
-
-  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-  </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
-  </script>
-
-
+<script src="../crud/php/main.js"></script>
 </body>
-
 </html>
